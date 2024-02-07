8416b522c07bb6afe1ff2a72962faec35954e486
Marcos Brito <marcos.brito0507@gmail.com>
2024-02-07 09:58:00 -0300

    feat(renderer): the real functions to render the sidebar

--- src/renderer/askama_renderer.rs
+++ src/renderer/askama_renderer.rs
@@ -50,6 +50,72 @@ impl AskamaRenderer {
 
         Ok(header.render()?)
     }
+
+    // The template engine makes difficult to renderer the sidebar, so we do the heavy lifting here
+    fn render_sidebar(&self) -> Result<String> {
+        let title = self.context.config.general.title.clone();
+        let items = self.context.content.summary.items.clone();
+        let mut table_of_contents = String::new();
+
+        for item in items.iter() {
+            match item {
+                Item::Chapter(chapter) => {
+                    table_of_contents.push_str(&self.render_sidebar_chapter(&chapter)?)
+                }
+                Item::Section(section) => {
+                    table_of_contents.push_str(&self.render_sidebar_section(&section)?)
+                }
+            }
+        }
+
+        let sidebar = Sidebar {
+            title: &title,
+            table_of_contents: &table_of_contents,
+        };
+
+        Ok(sidebar.render()?)
+    }
+
+    fn render_sidebar_chapter(&self, chapter: &Chapter) -> Result<String> {
+        // We don't care about indentation here. The css class takes care of it.
+        let subchapters = chapter
+            .subchapters
+            .iter()
+            .map(|item| match item {
+                Item::Chapter(chapter) => self.render_sidebar_chapter(&chapter),
+                _ => Ok(String::new()), // FIX: subchapters should not be an array of Item
+            })
+            .collect::<Result<Vec<String>>>()?
+            .join("");
+
+        let target = chapter
+            .content
+            .file_stem()
+            .unwrap()
+            .to_string_lossy()
+            .to_string();
+
+        let title = match self.context.config.general.enumerate {
+            true => format!("{} {}", chapter.number, chapter.title),
+            false => chapter.title.clone(),
+        };
+
+        let sidebar_chapter = SidebarChapter {
+            title: &title,
+            subchapters: &subchapters,
+            target: &(target + ".html"),
+        };
+
+        Ok(sidebar_chapter.render()?)
+    }
+
+    fn render_sidebar_section(&self, section: &Section) -> Result<String> {
+        let sidebar_section = SidebarSection {
+            title: &section.title,
+        };
+
+        Ok(sidebar_section.render()?)
+    }
 }
 
 impl Renderer for AskamaRenderer {
