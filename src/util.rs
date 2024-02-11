358ec0d14c5f66777d6d86c7eb16a8894fa4fc1d
Marcos Brito <marcos.brito0507@gmail.com>
2024-02-11 17:28:09 -0300

    feat(util): add method to deep copy a directory

--- src/util.rs
+++ src/util.rs
@@ -15,6 +15,27 @@ where
     Ok(())
 }
 
+pub fn copy_dir<P>(source: P, destination: P) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let source = source.as_ref();
+    let destination = destination.as_ref();
+
+    for entry in source.read_dir()? {
+        let entry = entry?;
+
+        if entry.file_type()?.is_dir() {
+            fs::create_dir(destination.join(entry.file_name()))?;
+            copy_dir(entry.path(), destination.join(entry.file_name()))?
+        } else {
+            fs::copy(entry.path(), destination.join(entry.file_name()))?;
+        }
+    }
+
+    Ok(())
+}
+
 pub fn remove_whitespace<S>(s: S) -> String
 where
     S: AsRef<str>,
