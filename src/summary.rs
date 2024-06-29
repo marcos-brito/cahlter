005b41e41b23f44e490eb04ace02c85a72d213f7
Marcos Brito <marcos.brito0507@gmail.com>
2024-06-29 14:04:15 -0300

    feat(summary): add support for summary file

--- src/summary.rs
+++ src/summary.rs
@@ -1,8 +1,10 @@
 mod file_tree_summarizer;
+mod summary_file;
 
 use crate::Item;
 use anyhow::Result;
 pub use file_tree_summarizer::FileTreeSummarizer;
+pub use summary_file::SummaryFileSummarizer;
 
 pub trait Summarizer {
     fn summarize(&self) -> Result<Summary>;
